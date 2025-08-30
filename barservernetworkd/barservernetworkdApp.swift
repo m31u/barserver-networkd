45cfd6a8f01ffb6f70d4a4c95ba4b09d76b8a041
Matthew MAsters <mattdmasters@gmail.com>
2025-08-30 16:56:06 +0800

    Initial Commit

---
+++ barservernetworkd/barservernetworkdApp.swift
@@ -0,0 +1,17 @@
+//
+//  barservernetworkdApp.swift
+//  barservernetworkd
+//
+//  Created by Matthew Masters on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct barservernetworkdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
