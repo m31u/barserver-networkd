2e88df3cb8db2db67616443e2f6ac378062dcbff
Matthew MAsters <mattdmasters@gmail.com>
2025-08-31 09:39:31 +0800

    initial commit

---
+++ barservernetworkd/WebSocketDaemonClient.swift
@@ -0,0 +1,101 @@
+import Foundation
+
+class WebSocketDaemonClient {
+    private var url: String
+    private var ws: URLSessionWebSocketTask?
+    private var currentState: () -> Void
+
+    init(_ url: String, _ currentStateCallback: @escaping () -> Void) {
+        currentState = currentStateCallback
+        self.url = url
+        connect(withURL: self.url)
+    }
+
+    func connect(withURL url: String) {
+        guard let url = URL(string: url) else {
+            print("Invalid url client not initialized")
+            return
+        }
+
+        ws = URLSession(configuration: .default).webSocketTask(with: url)
+
+        if let ws = ws {
+            ws.resume()
+            register()
+        }
+    }
+
+    func register() {
+        guard let ws = ws else {
+            print("couldn't register, Websocket task not initialized")
+            return
+        }
+
+        let data: [String: Any] = [
+            "type": "daemon",
+            "name": "NETWORK_DAEMON",
+        ]
+
+        guard let json = try? JSONSerialization.data(withJSONObject: data) else {
+            print("failed to create registration payload")
+            return
+        }
+
+        guard let payload = String(data: json, encoding: .utf8) else {
+            print("failed to create registration payload")
+            return
+        }
+
+        ws.send(URLSessionWebSocketTask.Message.string(payload)) { [self] error in
+            if let error = error {
+                print("error sending registration message \(error)")
+                return
+            }
+            currentState()
+            receive()
+        }
+    }
+
+    func send(data: [String: Any]) {
+        guard let ws = ws else {
+            print("couldn't send, Websocket task not initialized")
+            return
+        }
+
+        guard let json = try? JSONSerialization.data(withJSONObject: data) else {
+            print("couldn't serialize message")
+            return
+        }
+
+        guard let payload = String(data: json, encoding: .utf8) else {
+            print("couldn't serialize message")
+            return
+        }
+
+        ws.send(URLSessionWebSocketTask.Message.string(payload)) { error in
+            if let error = error {
+                print("error sending message \(error)")
+            }
+        }
+    }
+
+    func receive() {
+        guard let ws = ws else {
+            print("couldn't receive, Websocket task not initialized")
+            return
+        }
+
+        ws.receive { [self] result in
+            switch result {
+            case .success:
+                currentState()
+                receive()
+                break
+            case .failure:
+                connect(withURL: url)
+                break
+            }
+        }
+    }
+
+}
---
+++ barservernetworkd/main.swift
@@ -0,0 +1,155 @@
+import CoreLocation
+import CoreWLAN
+import Foundation
+
+enum WifiEvent {
+    case SSID_CHANGE
+    case WIFI_DISCONNECT
+    case WIFI_CONNECT
+}
+
+
+class WifiObserverPermissionManager {
+    private let manager = CLLocationManager()
+    private let wifiObserver = WiFiObserver()
+
+    var authorizationStatus: CLAuthorizationStatus {
+        return manager.authorizationStatus
+    }
+     
+    func startObserverWithPermissions() {
+        print("\(authorizationStatus)")
+        if authorizationStatus == .authorizedAlways {
+            print("Already authorized starting observer")
+            wifiObserver.start()
+            return
+        }
+        
+        manager.requestAlwaysAuthorization()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: startObserverWithPermissions)
+        print("requesting authorization")
+    }
+}
+
+let isScreenUnlocked = NSNotification.Name("com.apple.screenIsUnlocked")
+
+class WiFiObserver: CWEventDelegate {
+    private let client = CWWiFiClient.shared()
+    private let center = DistributedNotificationCenter.default()
+    private var unlockObserver: NSObjectProtocol?
+    private var wsDaemonClient: WebSocketDaemonClient?
+
+    init() {
+        client.delegate = self
+    }
+
+    func start() {
+        try! client.startMonitoringEvent(with: .linkDidChange)
+        try! client.startMonitoringEvent(with: .modeDidChange)
+        try! client.startMonitoringEvent(with: .ssidDidChange)
+
+        unlockObserver = center.addObserver(forName: isScreenUnlocked, object: nil, queue: .main) {
+            [self] _ in
+            if let interface = client.interface() {
+                postMode(withInterface: interface)
+                postSSID(withInterface: interface)
+            }
+        }
+        DispatchQueue.main.async(execute: waitForHeartbeat)
+    }
+
+    func waitForHeartbeat() {
+        guard let url = URL(string: "http://localhost:3000/heartbeat") else {
+            return
+        }
+        let req = URLRequest(url: url)
+
+        let task = URLSession.shared.dataTask(with: req) { [self] _, _, err in
+            if err != nil {
+                DispatchQueue.main.asyncAfter(
+                    deadline: .now() + 5.0, execute: waitForHeartbeat)
+                return
+            }
+            
+            wsDaemonClient = WebSocketDaemonClient("ws://localhost:3000/listen") { [self] in
+                if let interface = client.interface() {
+                    postMode(withInterface: interface)
+                    postSSID(withInterface: interface)
+                }
+            }
+        }
+
+        task.resume()
+    }
+
+    func sendInfo(event: WifiEvent, data: [String: Any]) {
+        guard let ws = wsDaemonClient else {
+            return
+        }
+        
+        let payload: [String: Any] = [
+            "type": "NETWORK_UPDATE_\(event)",
+            "data": data,
+        ]
+        
+        ws.send(data: payload)
+    }
+
+    func ssidDidChangeForWiFiInterface(withName interfaceName: String) {
+        postSSID(withName: interfaceName)
+    }
+
+    func linkDidChangeForWiFiInterface(withName interfaceName: String) {
+        postMode(withName: interfaceName)
+    }
+
+    func modeDidChangeForWiFiInterface(withName interfaceName: String) {
+        postMode(withName: interfaceName)
+    }
+
+    func postMode(withInterface interface: CWInterface) {
+        let mode = interface.interfaceMode()
+
+        if mode == .none {
+            sendInfo(event: .WIFI_DISCONNECT, data: ["connected": false])
+        }
+        if mode == .station {
+            sendInfo(event: .WIFI_CONNECT, data: ["connected": true])
+        }
+    }
+
+    func postMode(withName interfaceName: String) {
+        guard let interface = client.interface(withName: interfaceName) else {
+            print("couldn't get specified interface \(interfaceName)")
+            return
+        }
+        postMode(withInterface: interface)
+    }
+
+    func postSSID(withInterface interface: CWInterface) {
+        if interface.interfaceMode() == .station {
+            sendInfo(event: .SSID_CHANGE, data: ["ssid": interface.ssid() ?? "NO_PERMISSIONS"])
+        }
+    }
+
+    func postSSID(withName interfaceName: String) {
+        guard let interface = client.interface(withName: interfaceName) else {
+            print("couldn't get specified interface \(interfaceName)")
+            return
+        }
+        postSSID(withInterface: interface)
+    }
+
+    deinit {
+        try! client.stopMonitoringAllEvents()
+        if let obs = unlockObserver {
+            center.removeObserver(obs)
+        }
+    }
+}
+
+WifiObserverPermissionManager().startObserverWithPermissions()
+
+RunLoop.main.run()
+
